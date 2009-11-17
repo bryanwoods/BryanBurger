0acfc06f6cd339ca75a15ae1e154e20d0904600b
Bryan Woods <bryanwoods4e@gmail.com>
2009-11-17 17:27:31 -0500

    Removing old files

--- PyMan.py
+++
@@ -1,8 +0,0 @@
-from PyManMain import *
-
-if not pygame.font: print 'Warning, fonts disabled'
-if not pygame.mixer: print 'Warning, sound disabled'
-
-if __name__ == "__main__":
-  MainWindow = PyManMain()
-  MainWindow.MainLoop()
